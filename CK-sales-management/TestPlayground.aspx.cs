4effe010e7bc38bb57898289fd61fa47bbb9ffae
Unknown <tadikonda1@marshall.edu>
2017-10-26 21:28:52 -0400

    Tetsing Github Commit
    First Commit

---
+++ CK-sales-management/TestPlayground.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CK_sales_management
+{
+    public partial class TestPlayground : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            practiceGitHUbCommits();
+        }
+
+        private  void practiceGitHUbCommits()
+        {
+            int a = 5;
+            a++;
+            int b = a;
+        }
+    }
+}
